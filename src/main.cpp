e2642d86e00851ccfeef4fabf1783e99691b3764
Benoit Malenfant <benmalenfant@gmail.com>
2023-09-29 16:06:35 -0400

    add light developement environnement

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,21 @@
 #include <Arduino.h>
 
+#ifdef USE_MICRO_ROS
 #include "roboguard_microros.h"
+#endif
 
 void setup() {
   pinMode(PA_5, OUTPUT);
 
+  #ifdef USE_MICRO_ROS
   setup_micro_ros();
+  #endif
+  
 }
 
 void loop() {
   delay(500);
+  #ifdef USE_MICRO_ROS
   digitalWrite(PA_5,update_micro_ros());
+  #endif
 }
