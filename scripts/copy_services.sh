87a4348654d071437706677e1422f511bea2183d
Benoit Malenfant <benmalenfant@gmail.com>
2024-06-23 17:24:16 -0400

    fix path

--- scripts/copy_services.sh
+++ scripts/copy_services.sh
@@ -2,5 +2,5 @@
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-cp $script_dir/scripts/launch-micro-ros.service /etc/systemd/system/launch-micro-ros.service
-cp $script_dir/scripts/setup_can0.service /etc/systemd/system/setup_can0.service
+cp $script_dir/launch-micro-ros.service /etc/systemd/system/launch-micro-ros.service
+cp $script_dir/setup_can0.service /etc/systemd/system/setup_can0.service
