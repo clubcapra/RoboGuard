ad5be35fbf622bca3c1050e487ee622c493c0492
ili_the_butterfly <iliana.decarufel@gmail.com>
2025-06-08 20:04:12 -0400

    Fixed some typos and permissions

--- scripts/copy_services.sh
+++ scripts/copy_services.sh
@@ -2,5 +2,5 @@
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-cp $script_dir/launch-micro-ros.service /etc/user/system/launch-micro-ros.service
-cp $script_dir/setup_can0.service /etc/user/system/setup_can0.service
+sudo cp $script_dir/launch-micro-ros.service /etc/systemd/user/launch-micro-ros.service
+sudo cp $script_dir/setup_can0.service /etc/systemd/system/setup_can0.service
--- scripts/update-micro-ros.service.sh
+++ scripts/update-micro-ros.service.sh
@@ -1,3 +1,6 @@
 systemctl stop --user micro-ros.service
-cp /home/capra/RoboGuard/scripts/micro-ros.service /etc/systemd/user/micro-ros.service
+sudo cp /home/capra/RoboGuard/scripts/micro-ros.service /etc/systemd/user/micro-ros.service
+sudo chmod +x /etc/systemd/user/micro-ros.service
+sudo chown capra:capra /etc/systemd/user/micro-ros.service
+systemctl --user daemon-reload
 systemctl start --user micro-ros.service
